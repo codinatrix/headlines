4a8b9956822b50e6a771f8cd332bbe40d58beaf5
lala <whatwasthatonethen@gmail.com>
2014-02-23 15:06:08 +0100

    Deleted unused route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Headlines::Application.routes.draw do
   resources :headlines, only: [:create]
   
   root to: 'headlines#index'
-    
-  #get '/new', to: 'headlines#create', as: 'headlines_path'
   
 end
