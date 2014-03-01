08ef6c88cffa242a9f2854302789ec0a79191e5f
lala <whatwasthatonethen@gmail.com>
2014-03-02 00:11:51 +0100

    Form now slides in submission

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,12 @@ $(document)
 		var li = $("ul").children(":first").clone();
 		li.children(":first").text(data['content']);
 		li.children(":last").text(data['company']);
-		(li).prependTo$("ul").slideDown("slow");
+		
+		last_li = $("ul").children(":last");
+		last_li.remove();
+		
+		li.prependTo("ul").hide().slideDown("slow");
+		var test = 1;
 	})
 	.delegate('form[data-remote]', 'ajax:error', function(e, data, status, error) {
 		alert('error');
