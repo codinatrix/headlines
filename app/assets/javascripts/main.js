bb8ee121a666a02d969720ad67e354ef94304f55
lala <whatwasthatonethen@gmail.com>
2014-03-11 19:16:13 +0100

    Added - before company name

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,7 +5,7 @@ $(document)
 	.delegate('form[data-remote]', 'ajax:success', function(e, data, status, xhr){
 		var li = $("ul").children(":first").clone();
 		li.children(":first").text(data['content']);
-		li.children(":last").text(data['company']);
+		li.children(":last").text('-' + data['company']);
 		
 		last_li = $("ul").children(":last");
 		last_li.remove();
