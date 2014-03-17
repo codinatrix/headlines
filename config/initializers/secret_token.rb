6bfba4bbca17dc4197dd5faa7719bd3b7bd3f813
lala <whatwasthatonethen@gmail.com>
2014-03-17 15:58:53 +0100

    Added secret token to env for production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Headlines::Application.config.secret_key_base = '743f2da01e9ec3c58cbaa46f6126fb9a413e4245cd42f9a58c1fc37a399b5ba0fe6b7d99e73f8ac1a297fca0603cc193da476e33dfe097d830e1663062a16827'
+Headlines::Application.config.secret_key_base = ENV['SECRET_TOKEN'] || "localonly"
